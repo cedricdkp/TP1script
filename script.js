537a14ef95c8fbc076551ac238369bfc38ce45d9
cedric <ccdidkp@gmail.com>
2024-09-07 19:53:08 +0100

    depot script

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+/*let list = document.querySelectorAll(".navbar li");
+
+function activelink() {
+    list.forEach((item) => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach((item) => {
+    item.addEventListener("mouseover", activelink);
+});*/
+
+
+
+let navbar = document.querySelector(".navbar");
+let bicon = document.querySelector(".bicon");
+let main = document.querySelector(".main");
+let rest = document.querySelector(".rest");
+
+bicon . onclick = function () {
+    navbar.classList.toggle("active");
+    main.classList.toggle("active");
+    rest.classList.toggle("active")
+}
+
+
+
